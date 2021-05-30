be0027284bd9e9d65013aee59b6fc1196719e428
RekhuGopal <vrchinnarathod@gmail.com>
2021-05-30 16:18:26 +0530

    updated

--- AWSBoto3Hacks/AWSboto3DynamoDB-Operations.py
+++ AWSBoto3Hacks/AWSboto3DynamoDB-Operations.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 from pprint import pprint
 from decimal import Decimal
+import time
 
 
 client = boto3.client('dynamodb')
@@ -53,7 +54,7 @@ def put_movie(title, year, plot, rating):
                 "S": "{}".format(plot),
             },
             'rating': {
-                "S": "{}".format(rating),
+                "N": "{}".format(rating),
             }
         }
     )
@@ -61,10 +62,18 @@ def put_movie(title, year, plot, rating):
 
 ##Get a record in from DynamoDB table
 def get_movie(title, year):
-    table = client.Table('Movies')
-
     try:
-        response = table.get_item(Key={'year': year, 'title': title})
+        response = client.get_item(       
+                TableName='Movies',
+                Key={
+                        'year': {
+                                'N': "{}".format(year),
+                        },
+                        'title': {
+                                'S': "{}".format(title),
+                        }
+                    }
+                )
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
@@ -72,77 +81,80 @@ def get_movie(title, year):
 
 ## Update a record in DynamoDB table
 def update_movie(title, year, rating, plot, actors):
-    table = client.Table('Movies')
-
-    response = table.update_item(
+    response = client.update_item(
+        TableName='Movies',
         Key={
-            'year': year,
-            'title': title
+            'year': {
+                    'N': "{}".format(year),
+            },
+            'title': {
+                    'S': "{}".format(title),
+            }
         },
-        UpdateExpression="set info.rating=:r, info.plot=:p, info.actors=:a",
+        ExpressionAttributeNames={
+            '#R': 'rating',
+            '#P': 'plot',
+            '#A': 'actors'
+        },
         ExpressionAttributeValues={
-            ':r': Decimal(rating),
-            ':p': plot,
-            ':a': actors
+            ':r': {
+                'N': "{}".format(rating),
+            },
+            ':p': {
+                'S': "{}".format(plot),
+            },
+            ':a': {
+                'SS': actors,
+            }
         },
+        UpdateExpression='SET #R = :r, #P = :p, #A = :a',
         ReturnValues="UPDATED_NEW"
     )
     return response
 
 ## Increment an Atomic Counter in DynamoDB table
 def increase_rating(title, year, rating_increase):
-
-    table = client.Table('Movies')
-
-    response = table.update_item(
+    response = client.update_item(
+        TableName='Movies',
         Key={
-            'year': year,
-            'title': title
+            'year': {
+                    'N': "{}".format(year),
+            },
+            'title': {
+                    'S': "{}".format(title),
+            }
         },
-        UpdateExpression="set info.rating = info.rating + :val",
+        ExpressionAttributeNames={
+            '#R': 'rating'
+        },
         ExpressionAttributeValues={
-            ':val': Decimal(rating_increase)
+            ':r': {
+                'N': "{}".format(Decimal(rating_increase)),
+            }
         },
+        UpdateExpression='SET #R = #R + :r',
         ReturnValues="UPDATED_NEW"
     )
     return response
 
-## Update an Item (Conditionally) in DynamoDB table
-def remove_actors(title, year, actor_count):
-    table = client.Table('Movies')
-
-    try:
-        response = table.update_item(
-            Key={
-                'year': year,
-                'title': title
-            },
-            UpdateExpression="remove info.actors[0]",
-            ConditionExpression="size(info.actors) > :num",
-            ExpressionAttributeValues={':num': actor_count},
-            ReturnValues="UPDATED_NEW"
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
-            print(e.response['Error']['Message'])
-        else:
-            raise
-    else:
-        return response
-
 ## Delete an Item in DynamoDB table
 def delete_underrated_movie(title, year, rating):
-    table = client.Table('Movies')
-
     try:
-        response = table.delete_item(
+        response = client.delete_item(
+            TableName='Movies',
             Key={
-                'year': year,
-                'title': title
+                'year': {
+                    'N': "{}".format(year),
+                },
+                'title': {
+                    'S': "{}".format(title),
+                }
             },
-            ConditionExpression="info.rating <= :val",
+            ConditionExpression="rating <= :a",
             ExpressionAttributeValues={
-                ":val": Decimal(rating)
+                ':a': {
+                    'N': "{}".format(rating),
+                }
             }
         )
     except ClientError as e:
@@ -156,43 +168,35 @@ def delete_underrated_movie(title, year, rating):
 if __name__ == '__main__':
 
     ## Create DynamoDB
-    #movie_table = create_movie_table()
-    #print("Table status:{}".format(movie_table))
+    movie_table = create_movie_table()
+    print("Create DynamoDB succeeded............")
+    print("Table status:{}".format(movie_table))
+
+    time.sleep(30)
 
     ## Insert in to DynamoDB
     movie_resp = put_movie("The Big New Movie", 2015,"Nothing happens at all.", 0)
-    print("Put movie succeeded:")
+    print("Insert in to DynamoDB succeeded............")
     pprint(movie_resp, sort_dicts=False)
 
-'''
     ## Get an item from DynamoDB
     movie = get_movie("The Big New Movie", 2015,)
     if movie:
-        print("Get movie succeeded:")
+        print("Get an item from DynamoDB succeeded............")
         pprint(movie, sort_dicts=False)
 
     ## Update and item in  DynamoDB
     update_response = update_movie( "The Big New Movie", 2015, 5.5, "Everything happens all at once.",["Larry", "Moe", "Curly"])
-    print("Update movie succeeded:")
+    print("Update and item in  DynamoDB succeeded............")
     pprint(update_response, sort_dicts=False)
 
     ## Increment an Atomic Counter in DynamoDB
     update_response = increase_rating("The Big New Movie", 2015, 1)
-    print("Update movie succeeded:")
+    print("Increment an Atomic Counter in DynamoDB succeeded............")
     pprint(update_response, sort_dicts=False)
 
-    ## Update an Item (Conditionally) in DynamoDB
-    print("Attempting conditional update (expecting failure)...")
-    update_response = remove_actors("The Big New Movie", 2015, 3)
-    if update_response:
-        print("Update movie succeeded:")
-        pprint(update_response, sort_dicts=False)
-
     ## Delete an Item in DynamoDB table
-    print("Attempting a conditional delete...")
-    delete_response = delete_underrated_movie("The Big New Movie", 2015, 5)
+    delete_response = delete_underrated_movie("The Big New Movie", 2015, 7.5)
     if delete_response:
-        print("Delete movie succeeded:")
+        print("Delete an Item in DynamoDB table.........................")
         pprint(delete_response, sort_dicts=False)
-
-'''
