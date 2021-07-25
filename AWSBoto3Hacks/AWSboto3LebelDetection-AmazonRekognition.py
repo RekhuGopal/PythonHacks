10f2eb3658a65471e1ed7bdd3c0e46ea129e2562
RekhuGopal <vrchinnarathod@gmail.com>
2021-07-25 21:17:37 +0530

    Amazon Rekognition Service

---
+++ AWSBoto3Hacks/AWSboto3LebelDetection-AmazonRekognition.py
@@ -0,0 +1,41 @@
+import boto3
+
+def detect_labels(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=10)
+
+    print('Detected labels for ' + photo) 
+    print()   
+    for label in response['Labels']:
+        print ("Label: " + label['Name'])
+        print ("Confidence: " + str(label['Confidence']))
+        print ("Instances:")
+        for instance in label['Instances']:
+            print ("  Bounding box")
+            print ("    Top: " + str(instance['BoundingBox']['Top']))
+            print ("    Left: " + str(instance['BoundingBox']['Left']))
+            print ("    Width: " +  str(instance['BoundingBox']['Width']))
+            print ("    Height: " +  str(instance['BoundingBox']['Height']))
+            print ("  Confidence: " + str(instance['Confidence']))
+            print()
+
+        print ("Parents:")
+        for parent in label['Parents']:
+            print ("   " + parent['Name'])
+        print ("----------")
+        print ()
+    return len(response['Labels'])
+
+
+def main():
+    photo='Doha.jpg'
+    bucket='amazonrekognition-cqpocs'
+    label_count=detect_labels(photo, bucket)
+    print("Labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()
