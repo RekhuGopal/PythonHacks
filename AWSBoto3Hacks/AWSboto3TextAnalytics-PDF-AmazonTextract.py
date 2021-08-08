1854576aefa247f57a8035e62908b97bf58eaba2
RekhuGopal <vrchinnarathod@gmail.com>
2021-08-08 19:17:15 +0530

    AWS Texttract for PDF

---
+++ AWSBoto3Hacks/AWSboto3TextAnalytics-PDF-AmazonTextract.py
@@ -0,0 +1,60 @@
+import boto3
+import time
+
+def InvokeTextDetectJob(s3BucketName, objectName):
+    response = None
+    client = boto3.client('textract')
+    response = client.start_document_text_detection(
+            DocumentLocation={
+                      'S3Object': {
+                                    'Bucket': s3BucketName,
+                                    'Name': objectName
+                                }
+           })
+    return response["JobId"]
+
+def CheckJobComplete(jobId):
+    time.sleep(5)
+    client = boto3.client('textract')
+    response = client.get_document_text_detection(JobId=jobId)
+    status = response["JobStatus"]
+    print("Job status: {}".format(status))
+    while(status == "IN_PROGRESS"):
+        time.sleep(5)
+        response = client.get_document_text_detection(JobId=jobId)
+        status = response["JobStatus"]
+        print("Job status: {}".format(status))
+    return status
+
+def JobResults(jobId):
+    pages = []
+    client = boto3.client('textract')
+    response = client.get_document_text_detection(JobId=jobId)
+ 
+    pages.append(response)
+    print("Resultset page recieved: {}".format(len(pages)))
+    nextToken = None
+    if('NextToken' in response):
+        nextToken = response['NextToken']
+        while(nextToken):
+            response = client.get_document_text_detection(JobId=jobId, NextToken=nextToken)
+            pages.append(response)
+            print("Resultset page recieved: {}".format(len(pages)))
+            nextToken = None
+            if('NextToken' in response):
+                nextToken = response['NextToken']
+    return pages
+
+# Document
+s3BucketName = "demotextractcqpocs"
+documentName = "aws-overview.pdf"
+
+# Function invokes
+jobId = InvokeTextDetectJob(s3BucketName, documentName)
+print("Started job with id: {}".format(jobId))
+if(CheckJobComplete(jobId)):
+    response = JobResults(jobId)
+    for resultPage in response:
+        for item in resultPage["Blocks"]:
+            if item["BlockType"] == "LINE":
+                print ('\033[94m' + item["Text"] + '\033[0m')
