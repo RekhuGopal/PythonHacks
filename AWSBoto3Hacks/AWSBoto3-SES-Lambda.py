7849273b9ef4d8ee30f7911eaf1f42f71c74ce35
RekhuGopal <vrchinnarathod@gmail.com>
2022-01-16 21:19:13 +0530

    Updated

---
+++ AWSBoto3Hacks/AWSBoto3-SES-Lambda.py
@@ -0,0 +1,75 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def send_email():
+    SENDER = "vrchinnarathod@gmail.com"
+    RECIPIENT = "vrchinnarathod@gmail.com"
+
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-east-1"
+
+    # The subject line for the email.
+    SUBJECT = "This is test email for testing purpose..!!"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Hey Hi...\r\n"
+                "This email was sent with Amazon SES using the "
+                "AWS SDK for Python (Boto)."
+                )
+                
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+    <h1>Hey Hi...</h1>
+    <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES CQPOCS</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+        AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """            
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+        
+                        'Data': BODY_HTML
+                    },
+                    'Text': {
+        
+                        'Data': BODY_TEXT
+                    },
+                },
+                'Subject': {
+
+                    'Data': SUBJECT
+                },
+            },
+            Source=SENDER
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+
+def lambda_handler(event, context):
+    # TODO implement
+    send_email()
