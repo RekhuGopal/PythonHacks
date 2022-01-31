5f4e1b7f87218016a83e0483aeb03a458facf8c6
RekhuGopal <vrchinnarathod@gmail.com>
2022-01-31 18:51:20 +0530

    Updated

--- AWSBoto3Hacks/AWS-ETL-RDS-to-RedShift/CreateTable.sql
+++ AWSBoto3Hacks/AWS-ETL-RDS-to-RedShift/CreateTable.sql
@@ -1,9 +1,13 @@
-create table cqpocsredshiftdemo (
-  industry_name_ANZSIC varchar(100)    not null,
-  rme_size_grp     	   varchar(100)    not null,
-  variables        	   varchar(100)    not null
-)
+Create Table 
+========================
+CREATE TABLE Persons (
+    Id int,
+    Name varchar(255),
+    Email varchar(255)
+);
 
+Insert Table 
+========================
 INSERT INTO MyTable
   ( Id, Name, Email )
 VALUES
