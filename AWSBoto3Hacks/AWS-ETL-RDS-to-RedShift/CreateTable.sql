debdaa2a04abe7e317c4d6c1939ab2a40eb758a7
RekhuGopal <vrchinnarathod@gmail.com>
2022-01-31 18:48:14 +0530

    Updated

---
+++ AWSBoto3Hacks/AWS-ETL-RDS-to-RedShift/CreateTable.sql
@@ -0,0 +1,13 @@
+create table cqpocsredshiftdemo (
+  industry_name_ANZSIC varchar(100)    not null,
+  rme_size_grp     	   varchar(100)    not null,
+  variables        	   varchar(100)    not null
+)
+
+INSERT INTO MyTable
+  ( Id, Name, Email )
+VALUES
+  (1,'John', 'Lloyds Office'), 
+  (2,'Jane', 'Lloyds Office'), 
+  (3,'Billy', 'London Office'),
+  (4,'Miranda', 'Bristol Office');
