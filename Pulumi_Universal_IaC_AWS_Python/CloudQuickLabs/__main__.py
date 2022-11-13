7fd42cc254978b52f6c182740cad2a439ae9257a
RekhuGopal <vrchinnarathod@gmail.com>
2022-11-13 15:55:46 +0530

    Pulumi

---
+++ Pulumi_Universal_IaC_AWS_Python/CloudQuickLabs/__main__.py
@@ -0,0 +1,24 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+
+# Create an AWS resource (S3 Bucket)
+bucket = s3.Bucket('my-bucket-cloudquicklabs-001-demo',
+website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    )
+)
+
+bucketObject = s3.BucketObject(
+    'index.html',
+    acl='public-read',
+    content_type='text/html',
+    bucket=bucket.id,
+    source=pulumi.FileAsset('index.html')
+)
+
+# Export the name of the bucket
+pulumi.export('bucket_name', bucket.id)
+
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
