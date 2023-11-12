c435fe72d1042430df3c625679cc5d326b7b9e0e
RekhuGopal <vrchinnarathod@gmail.com>
2023-11-12 17:41:05 +0530

    Updated

---
+++ Azure_Synapse_Analytics/sqltest.py
@@ -0,0 +1,14 @@
+import pyodbc
+server = '<server>.database.windows.net'
+database = '<database>'
+username = '<username>'
+password = '{<password>}'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
--- ETL_AWS_DynamoDB_to_Amazon_Redshift_Using_KinesisFirehose/Table.sql
+++ ETL_AWS_DynamoDB_to_Amazon_Redshift_Using_KinesisFirehose/Table.sql
@@ -1,8 +1,23 @@
-CREATE TABLE EMPLOYEES (
-    Id varchar(255),
-    Name varchar(255),
-    City varchar(255),
-	Email varchar(255),
-    Designation varchar(255),
-	PhoneNumber varchar(255)
+CREATE TABLE Order (
+    order_id varchar(255),
+    product_id varchar(255),
+    customer_id varchar(255),
+	product varchar(255),
+    state_name varchar(255),
+	amount varchar,
+	currency varchar(255),
+	timestamp  varchar(255),
+	transaction_date varchar(255)
 );
+
+
+ CREATE TABLE Person(
+  PersonID int,
+  LastName varchar(255),
+  FirstName varchar(255),
+  Address varchar(255),
+  City varchar(255) 
+);
+
+INSERT INTO [dbo].[Persons] (PersonID, LastName, FirstName, Address_a, City)
+VALUES ('1001', 'Erichsen', 'Skagen', '4006,Church Street', 'NY');
