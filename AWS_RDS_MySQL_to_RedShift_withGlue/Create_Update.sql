8dba919bc6e63cdcab9afa55134e65c8e972c812
RekhuGopal <vrchinnarathod@gmail.com>
2024-01-19 15:24:04 +0530

    updated

---
+++ AWS_RDS_MySQL_to_RedShift_withGlue/Create_Update.sql
@@ -0,0 +1,15 @@
+create table cloudquicklabrdstbl (
+  industry_name_anzsic varchar(100)    not null,
+  rme_size_grp     	   varchar(100)    not null,
+  variables        	   varchar(100)    not null
+)
+
+INSERT INTO userdb.cloudquicklabrdstbl (industry_name_anzsic, rme_size_grp, variables)
+VALUES ('valuez1', 'valuez2', 'valuez3');
+
+INSERT INTO userdb.cloudquicklabrdstbl (industry_name_anzsic, rme_size_grp, variables)
+VALUES ('valuex1', 'valuex2', 'valuex3');
+
+INSERT INTO userdb.cloudquicklabrdstbl (industry_name_anzsic, rme_size_grp, variables)
+VALUES ('valuey1', 'valuey2', 'valuey3');
+
