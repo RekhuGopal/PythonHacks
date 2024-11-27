a93a51d743f85689d73a90be0526cc29f2054a85
RekhuGopal <vrchinnarathod@gmail.com>
2024-11-27 17:20:32 +0530

    Updated Changes

--- AWS_JsonUpload_To_S3_Bucket_With_API_Gateway_And_Lambda/InvokeAPIToUploadFile.py
+++ AWS_JsonUpload_To_S3_Bucket_With_API_Gateway_And_Lambda/InvokeAPIToUploadFile.py
@@ -1,22 +1,22 @@
 import requests
 
-url = "https://hhy0xsskr8.execute-api.us-west-2.amazonaws.com/v1/UploadJson"
+url = "<Gateway URL>"
 
-image_binary = {
+datadict = {
  "id" : "5645654645y65",
  "body-json" : {
-  "name": "refgdfgf",
-  "id": 24534,
-  "place" : "bng"
+  "name": "max",
+  "id": 2453243554,
+  "place" : "NewYork"
  }
 }
 
-APIGatewayKey = "GTUn6Fepnr8WWnLxYAT1c88HfWiRK7aN48qNyf0h"
+APIGatewayKey = "<API Gateway Key>"
 headers = {
-  'Content-Type': 'application/png',
+  'Content-Type': 'application/jsons',
   'x-api-key': APIGatewayKey
 }
 
-response = requests.request("POST", url, headers=headers, json=image_binary)
+response = requests.request("POST", url, headers=headers, json=datadict)
 
 print(response.text)
