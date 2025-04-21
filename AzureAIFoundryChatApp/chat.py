1de04fb56ca16d3488088c11a4d0929cd1bcb29d
RekhuGopal <vrchinnarathod@gmail.com>
2025-04-21 15:24:26 +0530

    Chat-gpt-4o-mini

---
+++ AzureAIFoundryChatApp/chat.py
@@ -0,0 +1,34 @@
+import os
+from openai import AzureOpenAI
+
+endpoint = "<Your API endpoint>"
+model_name = "<Your Model Name>"
+deployment = "<Your Deployment Name>"
+
+subscription_key = "<API-Key>"
+api_version = "2024-12-01-preview"
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I want learn AI and get a job , how should prepare?",
+        }
+    ],
+    max_tokens=4096,
+    temperature=1.0,
+    top_p=1.0,
+    model=deployment
+)
+
+print(response.choices[0].message.content)
