36d82db8fcbe90104ddfbc365a0351690c07e013
RekhuGopal <vrchinnarathod@gmail.com>
2025-05-23 13:24:37 +0530

    updated changes

---
+++ AzureAIFundamentaldemo/ComputervisionAI_ImageCaptionGenerator.py
@@ -0,0 +1,41 @@
+import os
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+
+# Set the values of your computer vision endpoint and computer vision key
+# as environment variables:
+try:
+    endpoint = "<YOUR ENDPOINT URL>"
+    key = "<YOUR KEY>"
+except KeyError:
+    print("Missing environment variable 'VISION_ENDPOINT' or 'VISION_KEY'")
+    print("Set them before running this sample.")
+    exit()
+
+# Create an Image Analysis client
+client = ImageAnalysisClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(key)
+)
+
+# Get a caption for the image. This will be a synchronously (blocking) call.
+result = client.analyze_from_url(
+    image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
+    visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
+    gender_neutral_caption=True,  # Optional (default is False)
+)
+
+print("Image analysis results:")
+# Print caption results to the console
+print(" Caption:")
+if result.caption is not None:
+    print(f"   '{result.caption.text}', Confidence {result.caption.confidence:.4f}")
+
+# Print text (OCR) analysis results to the console
+print(" Read:")
+if result.read is not None:
+    for line in result.read.blocks[0].lines:
+        print(f"   Line: '{line.text}', Bounding box {line.bounding_polygon}")
+        for word in line.words:
+            print(f"     Word: '{word.text}', Bounding polygon {word.bounding_polygon}, Confidence {word.confidence:.4f}")
